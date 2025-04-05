3b5fd210974ca78d87a6b402f8149817c99df3ef
Juan Mendieta <juanmsn@live.com>
2025-04-05 09:53:39 -0500

    Se agregó likes_comments_on_yt_shared_videos_by_timeline.py

---
+++ likes_comments_on_yt_shared_videos_by_timeline.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+page_url = os.getenv("PAGE_URL")
+post_title = os.getenv("POST_TITLE")
+chrome_user_data = r"C:\Users\juanm\AppData\Local\Google\Chrome\User Data"
+profiles = ["Profile 19", "Profile 24", "Profile 30", "Profile 31", "Profile 32", "Profile 33", "Profile 36", "Profile 37", "Profile 41", "Profile 42", "Profile 44", "Profile 46"] 
+n_scrolls = 2
+xpath_post_link_comentarios = f"//span[contains(normalize-space(text()), '{post_title}')]/following::span[contains(text(), 'comentarios')][1]"
+
+def scroll_comentarios():
+    xpath_comments_popup = "(//div[@role='dialog' and @aria-labelledby]/div/div/div/div)[2]"
+    popup = driver.find_element(By.XPATH, xpath_comments_popup) 
+    last_height = driver.execute_script("return arguments[0].scrollHeight", popup)
+    
+    while True:
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", popup)
+        time.sleep(3)
+        new_height = driver.execute_script("return arguments[0].scrollHeight", popup)
+        if new_height == last_height:
+            print("scrolls ended")
+            break
+        last_height = new_height 
+        print("scrolled")   
+        
+def like_comment():
+    current_like_path = "/following::div[contains(text(), 'Me gusta')][1]"
+    wait = WebDriverWait(driver, 30)
+    
+    with open("datos.txt", "r", encoding="utf-8") as file:
+        input_values = [line.strip() for line in file]    
+    
+    mas_relevantes = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Más relevantes"]')))
+    mas_relevantes.click()    
+    
+    todos_los_comentarios = driver.find_element(By.XPATH, '//span[text()="Todos los comentarios"]')
+    todos_los_comentarios.click()
+    driver.implicitly_wait(10)  
+    
+    scroll_comentarios()
+    
+    for value in input_values:
+        if value.endswith(".jpg"):
+            xpath_like = f"//img[contains(@src, '{value}')]{current_like_path}"
+        elif "segundos" in value:
+            xpath_like = f"//div[contains(normalize-space(@aria-label), '{value}')]{current_like_path}"
+        else:
+            xpath_like = f"//div[contains(normalize-space(text()), '{value}')]{current_like_path}"
+        try:
+            reaction = wait.until(
+                EC.element_to_be_clickable((By.XPATH, xpath_like))
+            )     
+            
+            style_attribute = reaction.get_attribute("style")
+            
+            if not style_attribute:       
+                driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", reaction)   
+                time.sleep(1)
+                reaction = wait.until(
+                    EC.element_to_be_clickable((By.XPATH, xpath_like))
+                )
+                driver.execute_script("arguments[0].click();", reaction)
+                print("Se hizo click en el boton me gusta")
+                print(value)
+            else:
+                print("Ya está likeado, no se hizo click")
+                print(value)
+        except TimeoutException:
+            print("No se encontró el elemento, pasando al siguiente.")
+            print(value)
+            continue 
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+            continue
+
+for i, profile in enumerate(profiles, start=1):
+    print(f"🟢 Abriendo Facebook con el perfil: {profile}")
+    options = Options()
+    options.add_argument(f"user-data-dir={chrome_user_data}")
+    options.add_argument(f"profile-directory={profile}")
+    # options.add_argument("--headless")     
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()    
+    driver.get(page_url)  
+    driver.implicitly_wait(10) 
+    
+    # Para acelerar la localizacion (ya se sabe de antemano la ubicación del post observandolo en el timeline)
+    for i in range(n_scrolls):
+        driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(3)
+
+    while True: 
+        link_comentarios = driver.find_elements(By.XPATH, xpath_post_link_comentarios)
+        if link_comentarios:
+            driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", link_comentarios[0])   
+            time.sleep(1)
+            link_comentarios[0].click()
+            break
+        driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(3) 
+
+    like_comment()
+    driver.quit()  
+    time.sleep(1)   # Time for waiting between changing profiles
+
+
